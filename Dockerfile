db4ddfce5a42f56f377887f39080a3698625af9c
VadymKurinnyi <Vadym.kurinnyi@gmail.com>
2024-04-15 23:45:07 +0100

    create keys

--- Dockerfile
+++ Dockerfile
@@ -1,38 +1,8 @@
-# Start with the official Rust image
-FROM rust:latest as builder
+FROM arm32v7/rust:1.77
 
-# Add ARMv7 target architecture for Rust
-RUN rustup target add armv7-unknown-linux-gnueabihf
+WORKDIR /usr/src/stryboh
+COPY . .
 
-# Enable `armhf` architecture and install dependencies
-RUN dpkg --add-architecture armhf && \
-    apt-get update && \
-    apt-get install -y crossbuild-essential-armhf pkg-config libssl-dev:armhf
+RUN cargo install --path .
 
-
-# Create a new binary project
-RUN USER=root cargo new --bin rust_arm_app
-WORKDIR /rust_arm_app
-
-# Copy your source code
-COPY ./src ./src
-COPY ./Cargo.toml ./Cargo.toml
-
-# Configure pkg-config for cross-compilation
-ENV PKG_CONFIG_ALLOW_CROSS=1
-ENV PKG_CONFIG_PATH=/usr/lib/arm-linux-gnueabihf/pkgconfig
-
-# Set OpenSSL environment variables for cross-compilation
-ENV OPENSSL_DIR=/usr/include/arm-linux-gnueabihf
-ENV OPENSSL_LIB_DIR=/usr/lib/arm-linux-gnueabihf
-ENV OPENSSL_INCLUDE_DIR=/usr/include/arm-linux-gnueabihf
-
-# Build your project for ARMv7
-RUN cargo build --release --target=armv7-unknown-linux-gnueabihf
-
-# Use a minimal runtime image that supports ARMv7
-FROM arm32v7/debian:buster-slim
-COPY --from=builder /rust_arm_app/target/armv7-unknown-linux-gnueabihf/release/rust_arm_app .
-
-# Command to run your binary
-CMD ["./rust_arm_app"]
+CMD ["stryboh"]
--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,18 @@ use azeventhubs::consumer::{
     EventHubConsumerClient, EventHubConsumerClientOptions, ReadEventOptions,
 };
 use dotenv::dotenv;
-use fred::{prelude::*, types::Timestamp};
+use fred::{
+    prelude::*,
+    types::{Encoding, Timestamp},
+};
 use futures_util::StreamExt;
-use std::env;
+use std::{collections::HashMap, env};
 use telemetry::Telemetry;
 mod telemetry;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     dotenv().ok();
-
     let mut consumer_client = EventHubConsumerClient::new_from_connection_string(
         EventHubConsumerClient::DEFAULT_CONSUMER_GROUP_NAME, //"viter-consumer".to_string(),
         env::var("EVENT_HUB_CONNNECTION_STRING")
@@ -29,37 +31,43 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .await?;
 
     let config = RedisConfig::from_url(&env::var("REDIS_URL").expect("REDIS_URL not specifyed"))?;
-    
-    let client = Builder::from_config(config).build()?; 
+
+    let client = Builder::from_config(config).build()?;
     client.init().await?;
-    // client.ts_create(key, retention, encoding, chunk_size, duplicate_policy, labels) // todo!!!
-    
-    // Receive 30 events
+
     let mut counter = 0;
+    let mut key_cache = HashMap::<String, String>::new();
     while let Some(event) = stream.next().await {
         let event = event?;
         let body = event.body()?;
         match serde_json::from_slice::<Telemetry>(body) {
             Ok(telemetry) => {
                 if let None = telemetry.device_id {
-                    print!("Unknown device send message")
+                    println!("Unknown device send message")
                 }
                 if let None = telemetry.time_stamp {
-                    print!(
+                    println!(
                         "Device {:?} sends message without TimeStamp",
                         telemetry.device_id
                     )
                 }
-                let t_key = format!("temperature:{}", telemetry.device_id.clone().unwrap_or_default());
-                let h_key = format!("humidity:{}", telemetry.device_id.unwrap_or_default());
-                
-                let ts: Timestamp = Timestamp::from(telemetry.time_stamp.unwrap_or_default() * 1000);
-                let samples: [(RedisKey, Timestamp, f64); 2] = [
-                    (RedisKey::try_from(t_key)?,
-                    ts, telemetry.temperature), 
-                    (RedisKey::try_from(h_key)?,
-                    Timestamp::from(telemetry.time_stamp.unwrap_or_default() * 1000), telemetry.humidity), ];
-                client.ts_madd(samples).await?;
+                let device_id = telemetry.device_id.unwrap_or(String::from("Unknown"));
+                let t_key = ensure_key(&client, &mut key_cache, &device_id, "temperature").await?;
+                let h_key = ensure_key(&client, &mut key_cache, &device_id, "humidity").await?;
+
+                let ts: Timestamp =
+                    Timestamp::from(telemetry.time_stamp.unwrap_or_default() * 1000);
+
+                let mut value_pairs: Vec<(RedisKey, Timestamp, f64)> = Vec::new();
+                value_pairs.push((RedisKey::try_from(t_key)?, ts.clone(), telemetry.temperature));
+                value_pairs.push((RedisKey::try_from(h_key)?, ts.clone(), telemetry.humidity));
+
+                if let Some(pressure) =telemetry.pressure{
+                    let p_key = ensure_key(&client, &mut key_cache, &device_id, "pressure").await?;
+                    value_pairs.push((RedisKey::try_from(p_key)?, ts.clone(), pressure));
+                }
+
+                client.ts_madd(value_pairs).await?;
             }
             Err(e) => {
                 println!("{e}");
@@ -67,7 +75,6 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                 println!("{}", value);
             }
         }
-        // let value = std::str::from_utf8(body)?;
 
         println!("counter: {}", counter);
         counter += 1;
@@ -79,4 +86,42 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     consumer_client.close().await?;
 
     Ok(())
+}
+
+async fn ensure_key(
+    client: &RedisClient,
+    map: &mut HashMap<String, String>,
+    device_id: &str,
+    telemetry: &str,
+) -> Result<String, Box<dyn std::error::Error>> {
+    let key = format!("{}:{}", telemetry, device_id);
+
+    if map.contains_key(&key) {
+        return Ok(key);
+    }
+    if client.exists(&key).await? {
+        map.insert(key.clone(), String::from(""));
+        return Ok(key);
+    }
+    let retention = Some(365 * 24 * 60 * 60 * 1000);
+    let encoding = Some(Encoding::Compressed);
+    let chunk_size = None;
+    let duplicate_policy = Some(fred::types::DuplicatePolicy::Last);
+    let labels = HashMap::from([("id", device_id), ("telemetry", telemetry)]);
+    println!("creating a new timeseries {}", &key);
+
+    client
+        .ts_create(
+            &key,
+            retention,
+            encoding,
+            chunk_size,
+            duplicate_policy,
+            labels,
+        )
+        .await?;
+    println!("{} has been created", &key);
+    map.insert(key.clone(), String::from(""));
+
+    return Ok(key);
 }
